e156bbb00417fe77d7f3f97f81e8a70a103ee763
EgorBo <egorbo@gmail.com>
2023-07-14 15:31:54 +0200

    Fix parser

--- BotCommands.cs
+++ BotCommands.cs
@@ -3,6 +3,7 @@ using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using static Constants;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GoldChatBot;
 
@@ -157,22 +158,33 @@ public record Command(string Name,
 
 public static class TelegramExtensions
 {
-    private static List<MessageEntity> ExtractCodeSnippets(string message)
+    private static List<MessageEntity> ParseMessageEntity(ref string message)
     {
-        var snippets = new List<MessageEntity>();
+        var entities = new List<MessageEntity>();
         var regex = new Regex(@"```(.*?)```", RegexOptions.Singleline);
         var matches = regex.Matches(message);
         foreach (Match match in matches)
         {
-            snippets.Add(new MessageEntity { Length = match.Length, Offset = match.Index, Type = MessageEntityType.Code});
+            entities.Add(new MessageEntity
+                {
+                    Length = match.Length, 
+                    Offset = match.Index, 
+                    Type = MessageEntityType.Code
+                });
         }
-        return snippets;
+
+        if (entities.Count > 0)
+        {
+            // Remove backticks from the message
+            message = new Regex("```").Replace(message, "");
+        }
+        return entities; 
     }
 
 
     public static Task ReplyAsync(this ITelegramBotClient client, Message msg, string text)
     {
-        List<MessageEntity> entities = ExtractCodeSnippets(text);
+        List<MessageEntity> entities = ParseMessageEntity(ref text);
         return client.SendTextMessageAsync(chatId: msg.Chat, replyToMessageId: msg.MessageId, entities: entities, text: text);
     }
 }
