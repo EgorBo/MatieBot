c0e6314e5dd5cc582e7d6984e4254178cc079a1e
EgorBo <egorbo@gmail.com>
2023-10-30 15:25:20 +0100

    try parse again

--- BotCommands.cs
+++ BotCommands.cs
@@ -266,34 +266,9 @@ public record Command(string Name,
 
 public static class TelegramExtensions
 {
-    private static List<MessageEntity> ParseMessageEntity(ref string message)
-    {
-        var entities = new List<MessageEntity>();
-        var regex = new Regex(@"```(.*?)```", RegexOptions.Singleline);
-        var matches = regex.Matches(message);
-        foreach (Match match in matches)
-        {
-            entities.Add(new MessageEntity
-                {
-                    Length = match.Length, 
-                    Offset = match.Index, 
-                    Type = MessageEntityType.Code
-                });
-        }
-
-        if (entities.Count > 0)
-        {
-            // Remove backticks from the message
-            message = new Regex("```").Replace(message, "   ");
-        }
-        return entities; 
-    }
-
-
     public static Task ReplyAsync(this ITelegramBotClient client, Message msg, string text)
     {
-        List<MessageEntity> entities = ParseMessageEntity(ref text);
-        return client.SendTextMessageAsync(chatId: msg.Chat, replyToMessageId: msg.MessageId, entities: entities, text: text);
+        return client.SendTextMessageAsync(chatId: msg.Chat, replyToMessageId: msg.MessageId, parseMode: ParseMode.Markdown, text: text);
     }
 
     public static Task ReplyWithImageAsync(this ITelegramBotClient client, Message msg, string url)
